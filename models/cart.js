75b15295d39e9ada2b928a783b1aabfda1b12234
Owen <owenfok119@gmail.com>
2022-08-10 21:54:19 +0800

    update del

--- models/cart.js
+++ models/cart.js
@@ -13,6 +13,17 @@ module.exports = function Cart(oldCart){
         this.totalQty++;
         this.totalPrice += storedItem.item.price;
     };
+
+    this.add = function(item, id){
+        var storedItem = this.items[id];
+        if(!storedItem){
+            storedItem = this.items[id] = {item: item, qty: 0, price: 0};
+        }
+        storedItem.qty--;
+        storedItem.price = storedItem.item.price * storedItem.qty;
+        this.totalQty--;
+        this.totalPrice -= storedItem.item.price;
+    };
     
     this.generateArray = function(){
         var arr = [];
